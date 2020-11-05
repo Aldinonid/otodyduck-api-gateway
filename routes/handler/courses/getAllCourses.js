6dd49932ddef82c4cf35e87b67c60c7347716e00
Aldinonid <aldino.efendi7@gmail.com>
2020-11-06 02:24:42 +0700

    OTO-7 - Impact from search class

--- routes/handler/courses/getAllCourses.js
+++ routes/handler/courses/getAllCourses.js
@@ -6,13 +6,14 @@ const api = apiAdapter(URL_SERVICE_COURSE);
 module.exports = async (req, res) => {
   const status = req.query.status;
   try {
-    if (status) {
-      const getCourse = await api.get(`/api/courses?status=${status}`);
-      return res.json(getCourse.data);
-    } else {
-      const getCourse = await api.get("/api/courses");
-      return res.json(getCourse.data);
-    }
+    const courses = await api.get("/api/courses", {
+      params: {
+        ...req.query,
+      },
+    });
+
+    const getCourse = courses.data;
+    return res.json(getCourse.data);
   } catch (err) {
     if (err.code === "ECONNREFUSED") {
       return res
