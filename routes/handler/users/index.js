80f55fa9c593f7e4c21ffcc5c4ea21645ba9b95d
Aldinonid <aldino.efendi7@gmail.com>
2020-12-20 00:26:11 +0700

    OTO-46 - API Gateway Done

--- routes/handler/users/index.js
+++ routes/handler/users/index.js
@@ -3,13 +3,19 @@ const logout = require("./logout");
 const update = require("./update");
 const destroy = require("./destroy");
 const getUser = require("./getUser");
+const getAllUsers = require("./getAllUsers");
+const getOneUser = require("./getOneUser");
 const register = require("./register");
+const edit = require("./edit");
 
 module.exports = {
   register,
   login,
   update,
   getUser,
+  getAllUsers,
+  getOneUser,
   logout,
   destroy,
+  edit,
 };
--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,15 @@ const userHandler = require("./handler/users");
 const verifyToken = require("../middlewares/verifyToken");
 
 //? GET, POST, PUT, DELETE ?//
-router.get("/", verifyToken, userHandler.getUser);
+router.get("/user", verifyToken, userHandler.getUser);
+router.get("/", userHandler.getAllUsers);
+router.get("/:id", userHandler.getOneUser);
 
 router.post("/register", userHandler.register);
 router.post("/login", userHandler.login);
 router.post("/logout", verifyToken, userHandler.logout);
 
+router.put("/:id", userHandler.edit);
 router.put("/", verifyToken, userHandler.update);
 
 router.delete("/:id", userHandler.destroy);
